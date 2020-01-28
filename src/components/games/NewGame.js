9198d7246b685523c8bc7fb915b78b5c6af409f9
divakaivan <54508530+divakaivan@users.noreply.github.com>
2020-01-28 21:48:01 +0000

    adding modal for invalid champions

--- src/components/games/NewGame.js
+++ src/components/games/NewGame.js
@@ -2,13 +2,12 @@ import React, {useState} from "react";
 import "../../stylesheets/NewGame.css";
 import validChampions from "./CheckChampionValid";
 import ChampionPicked from "./ChampionPicked";
+import {Modal, Button} from "react-bootstrap";
 
 const NewGame = () => {
     const [champion, setChampion] = useState("");
     const [championsList, setChampionsList] = useState([]);
     const [invalidChampion, setInvalidChampion] = useState(false);
-    const [role, setRole] = useState("");
-    const [side, setSide] = useState("");
 
     const handleSubmit = e => {
         e.preventDefault();
@@ -18,45 +17,44 @@ const NewGame = () => {
             setInvalidChampion(true);
         }
     };
-    console.log(role);
+
     return (
         <div className="text-white mt-5 container">
             <div className="row center-block align-items-center text-center">
                 <div className="col-md-2">
                     {championsList[0] ? <ChampionPicked champion={championsList[0]}/> : (<h3>Top lane<br/></h3>)}
-                    {championsList[1] && role === "Jungle" && side === "Blue" ? <ChampionPicked champion={championsList[1]}/> : (<h3>Jungle<br/></h3>)}
-                    {championsList[2] && role === "Mid" && side === "Blue" ? <ChampionPicked champion={championsList[2]}/> : (<h3>Mid lane<br/></h3>)}
-                    {championsList[3] && role === "Bot" && side === "Blue" ? <ChampionPicked champion={championsList[3]}/> : (<h3>Bot lane<br/></h3>)}
-                    {championsList[4] && role === "Support" && side === "Blue" ? <ChampionPicked champion={championsList[4]}/> : (<h3>Support<br/></h3>)}
+                    {championsList[1] ? <ChampionPicked champion={championsList[1]}/> : (<h3>Jungle<br/></h3>)}
+                    {championsList[2] ? <ChampionPicked champion={championsList[2]}/> : (<h3>Mid lane<br/></h3>)}
+                    {championsList[3] ? <ChampionPicked champion={championsList[3]}/> : (<h3>Bot lane<br/></h3>)}
+                    {championsList[4] ? <ChampionPicked champion={championsList[4]}/> : (<h3>Support<br/></h3>)}
                 </div>
                 <div className="col-md-8">
-                    {invalidChampion ? (<div><h4 style={{color: "red"}}>Invalid Champion</h4><button onClick={()=>setInvalidChampion(false)}>OK</button></div>) : null}
-                    <div className="row">
-                        <div className="col-md-2">
-                            <form>
-                                <input type="radio" name="champ" value="Top" onClick={()=>setRole("Top")}/>Top<br/>
-                                <input type="radio" name="champ" value="Jungle" onClick={()=>setRole("Jungle")}/>Jungle<br/>
-                                <input type="radio" name="champ" value="Jungle" onClick={()=>setRole("Mid")}/>Mid<br/>
-                                <input type="radio" name="champ" value="Jungle" onClick={()=>setRole("Bot")}/>Bot<br/>
-                                <input type="radio" name="champ" value="Jungle" onClick={()=>setRole("Support")}/>Support<br/>
-                            </form>
-                        </div>
-                        <div className="col-md-2">
-                            <form>
-                                <input type="radio" name="champ" value="Top" onClick={()=>setSide("Blue")}/>Blue<br/>
-                                <input type="radio" name="champ" value="Jungle" onClick={()=>setRole("Red")}/>Red<br/>
-                            </form>
-                        </div>
-                    </div>
-                    <input type="text" value={champion} onChange={e=>setChampion(e.target.value)}/>
-                    <input type="submit" onClick={handleSubmit}/>
+                    {invalidChampion ?
+                        (
+                            <Modal animation={true} show={invalidChampion}>
+                                <Modal.Header closeButton>
+                                    <Modal.Title>Modal heading</Modal.Title>
+                                </Modal.Header>
+                                <Modal.Body>You entered an invalid champion: <strong>{champion}</strong></Modal.Body>
+                                <Modal.Footer>
+                                    <Button variant="secondary" onClick={() => setInvalidChampion(false)}>
+                                        Close
+                                    </Button>
+                                </Modal.Footer>
+                            </Modal>
+                        )
+                        : null}
+                    <h5>Input champions (left side from top to bottom then right side)</h5>
+                    <input type="text" className="input-champ" value={champion} onChange={e=>setChampion(e.target.value)}/>
+                    <br/>
+                    <Button variant="primary" type="submit" onClick={handleSubmit}>Submit</Button>
                 </div>
                 <div className="col-md-2">
-                    {championsList[5] && role === "Top" && side === "Red" ? <ChampionPicked champion={championsList[5]}/> : (<h3>Top lane<br/></h3>)}
-                    {championsList[6] && role === "Jungle" && side === "Red" ? <ChampionPicked champion={championsList[6]}/> : (<h3>Jungle<br/></h3>)}
-                    {championsList[7] && role === "Mid" && side === "Red" ? <ChampionPicked champion={championsList[7]}/> : (<h3>Mid lane<br/></h3>)}
-                    {championsList[8] && role === "Bot" && side === "Red" ? <ChampionPicked champion={championsList[8]}/> : (<h3>Bot lane<br/></h3>)}
-                    {championsList[9] && role === "Support" && side === "Red" ? <ChampionPicked champion={championsList[9]}/> : (<h3>Support<br/></h3>)}
+                    {championsList[5] ? <ChampionPicked champion={championsList[5]}/> : (<h3>Top lane<br/></h3>)}
+                    {championsList[6] ? <ChampionPicked champion={championsList[6]}/> : (<h3>Jungle<br/></h3>)}
+                    {championsList[7] ? <ChampionPicked champion={championsList[7]}/> : (<h3>Mid lane<br/></h3>)}
+                    {championsList[8] ? <ChampionPicked champion={championsList[8]}/> : (<h3>Bot lane<br/></h3>)}
+                    {championsList[9] ? <ChampionPicked champion={championsList[9]}/> : (<h3>Support<br/></h3>)}
                 </div>
             </div>
         </div>
